a8679432d55543b34f2ff051bdf66d7de34d01de
Marat <91463020+TheAblacFuck@users.noreply.github.com>
2022-12-09 22:14:05 +0500

    upload project

---
+++ Reg.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+
+<html>
+    
+      <title>JDM_Registration</title>
+      <link rel="stylesheet" href="Reg.css">
+    <body>
+
+        <video autoplay muted loop id="myVideo">
+  <source src="auth.mp4" type="video/mp4">
+</video>
+        
+        <div id="head" >
+            
+          <a href="Main.php"><image src="log.png"></image></a>
+            <p></p>
+            <p id="logoname"><font size="20" color="White" face="Old English Text MT">JDM</font></p>
+            <p id="NamePage"><font size="20" color="White" face="Old English Text MT">Регистрация на JDM</font></p>
+        </div>
+        <div class="content3">
+       
+
+<div id="wrapper">
+    <form id="signin" method="" action="" autocomplete="off">
+        <input type="text" id="user" name="user" placeholder="Логин" />
+        <input type="password" id="pass" name="pass" placeholder="Пароль" />
+        <input type="password" id="pass" name="pass" placeholder="Пароль еще раз" />
+        <p><button class="sozdat">Создать</button></p>
+    </form>
+</div>
+</div>
+    </body>
+    </html>
